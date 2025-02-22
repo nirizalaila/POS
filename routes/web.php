937f70ff1b90332f56379ca68f55a04e657bad14
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-02-22 11:56:45 +0700

    Menambahkan Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage'])->name('category.food-beverage');
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth'])->name('category.beauty-health');
+    Route::get('/home-care', [ProductController::class, 'homeCare'])->name('category.home-care');
+    Route::get('/baby-kid', [ProductController::class, 'babyKid'])->name('category.baby-kid');
 });
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show'])->name('user.show');
+
+Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
