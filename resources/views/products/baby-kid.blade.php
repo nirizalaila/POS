12d778aaa9b9561a62b35aaee6d9c13ff9aebd75
Niriza Lailaiumi Hidayat <nirizalailaumi1@gmail.com>
2025-02-22 12:43:46 +0700

    Membuat Product View

---
+++ resources/views/products/baby-kid.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Beauty & Health</title>
+    <body>
+        <h1>Daftar Produk: Beauty & Health</h1>
+        <ul>
+            <li>🍼 Susu Formula - Rp 50.000</li>
+            <li>👶 Pampers Bayi - Rp 75.000</li>
+            <li>🎒 Tas Sekolah Anak - Rp 100.000</li>
+            <li>🚗 Mainan Mobil-mobilan - Rp 40.000</li>
+        </ul>
+    </body>
+</html>
